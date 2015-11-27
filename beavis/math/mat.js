65b93034f9bedf6e4c6a00d7b5d574b1e2a06a0b
diklo <samuj986@newschool.edu>
2015-11-27 01:18:32 +0000

    msg ...

--- beavis/math/mat.js
+++ beavis/math/mat.js
@@ -1,44 +1,30 @@
-var things = ["bpa.jpg", "lo.jpg"];
-var randomThing = things[Math.floor(Math.random()*things.length)];
-console.log(randomThing);
-$("body").append("<img src='" + randomThing + "'>");
+var things =("bpa.jpg", "lo.jpg");
+var ings=("winn.jpg", "dx.jpg");
+var randomThing = things[Math.floor(Math.random()*things.length)]; //2
+   
+   $("body").append('<div id="buttonDIV"> </div>');
 
 
-// var words = ["hello","world","lorem","ipsum"];
 
-// for (var increment = 0; increment < 100; increment++) {
-//   $("body").append('<div class="button"></div>');
-// }
-
-// var randomThing = things[Math.floor(Math.random()*4)];
-
-// $("body").append("<img src='img/" + randomThing + "'>");
-
-
-// function makePattern() {
-//     $(".button").each(function() {
-//   var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
-//   var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
-//   $(this).css("top",topPosition).css("left",leftPosition);
-//   var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-//   $(this).css("background-color",newColor);
-//   var newSize = Math.floor(Math.random()*300) + "px";
-//   $(this).css("width",newSize).css("height",newSize);
-
-// });
-
-// }
-
-// $(window).resize(function() {
-//   makePattern();
-// });
-
-// $(".activate").click(function(){
-//   makePattern();
-// })
+$(".buttonDIV").mouseover(function(){
+    $(".thing").css("background-color", "slategray");
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*6902037595864385).toString(20);
+  $(this).css("border-color",newColor);
+});
 
 
-// for (var myPoem = 0; myPoem < 5; myPoem++) {
-//   $(".poem").append(words[Math.floor(Math.random()*words.length)]  + " ");
-  
-// }
+$(window).click(function() {
+    $(".link").each(function() {
+        var leftPosition = Math.floor(Math.random()*($(window).height()-60)) + "px";
+        var topPosition = Math.floor(Math.random()*($(window).width()-50)) + "px";
+        var topPosition = Math.floor(Math.random()*($(window).width()-50)) + "px";
+        $(this).css("left",leftPosition).css("top",topPosition);
+    });
+}); 
+   
+   
+
+$("body").css("background-image", "url(img/winn.jpg)");
